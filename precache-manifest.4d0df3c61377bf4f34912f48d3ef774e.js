4ba79845543dc591435bac59494232a9bb044557
eleex <eleex1988@gmail.com>
2020-04-11 22:20:30 +0300

    Updates

---
+++ precache-manifest.4d0df3c61377bf4f34912f48d3ef774e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e18c621a219e26d280e9e812e4cc05a",
+    "url": "/react-redux-stats/index.html"
+  },
+  {
+    "revision": "d90afa3022592008ad0d",
+    "url": "/react-redux-stats/static/css/main.1b323230.chunk.css"
+  },
+  {
+    "revision": "e4565801104fb8a7b452",
+    "url": "/react-redux-stats/static/js/2.0f0884e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-stats/static/js/2.0f0884e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90afa3022592008ad0d",
+    "url": "/react-redux-stats/static/js/main.95f853e9.chunk.js"
+  },
+  {
+    "revision": "984f7a9442eb87457e36",
+    "url": "/react-redux-stats/static/js/runtime-main.d221ca5f.js"
+  },
+  {
+    "revision": "ade5b2e9a6a36e840bda27fc984b0bd4",
+    "url": "/react-redux-stats/static/media/left-arrow-disabled.ade5b2e9.svg"
+  },
+  {
+    "revision": "ea86284d76133c736ca89bbd2baa3f7d",
+    "url": "/react-redux-stats/static/media/left-arrow-hover.ea86284d.svg"
+  },
+  {
+    "revision": "f205d21c8e115cdd26e7dbe9d649033b",
+    "url": "/react-redux-stats/static/media/left-arrow.f205d21c.svg"
+  },
+  {
+    "revision": "d7f2913ba5f39717d76efb3d1ddeeaf3",
+    "url": "/react-redux-stats/static/media/right-arrow-disabled.d7f2913b.svg"
+  },
+  {
+    "revision": "68992893f4a56c373144a1db007dff7f",
+    "url": "/react-redux-stats/static/media/right-arrow-hover.68992893.svg"
+  },
+  {
+    "revision": "e6100bed41bf6001eb3abc679a9b44ed",
+    "url": "/react-redux-stats/static/media/right-arrow.e6100bed.svg"
+  }
+]);
